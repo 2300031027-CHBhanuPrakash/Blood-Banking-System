a39fd83b7e022f9abee3d0185e9423c88828319e
2300031027-CHBhanuPrakash <bp4049760@gmail.com>
2025-11-08 08:25:42 +0530

    Fix routing for GitHub Pages by adding basename to BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,11 @@ import App from './App'
 import Dashboard from './Dashboard'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter >
+  <BrowserRouter basename="/Blood-Banking-System">
   <Routes>
   <Route path='/' element={<App/>} />
   <Route path='/dashboard'element={<Dashboard/>} />
   </Routes>
-    
+
   </BrowserRouter>,
 )
